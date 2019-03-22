1a8601569a8663064622858d36fccee6d2c39225
farewell <wzzaptx@outlook.com>
2019-03-22 13:14:48 +0800

    project init

---
+++ src/main/java/com/wzz/mmall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.wzz.mmall.dao;
+
+import com.wzz.mmall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
---
+++ src/main/java/com/wzz/mmall/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.wzz.mmall.dao;
+
+import com.wzz.mmall.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
